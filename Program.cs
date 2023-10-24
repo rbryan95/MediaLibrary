35fb5557acc728a01562b4b093820bf32a2065fd
rbryan95 <berd1495@gmail.com>
2023-10-24 17:40:25 -0500

    Create FileScrubber - Create Static class / method for scrubbing the movies.csv file - Update program to utilize the static method to scrub the file - Add *.csv to .gitignore

--- Program.cs
+++ Program.cs
@@ -7,37 +7,7 @@ string path = Directory.GetCurrentDirectory() + "\\nlog.config";
 var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
 logger.Info("Program started");
 
-Movie movie = new Movie
-{
-    mediaId = 123,
-    title = "Greatest Movie Ever, The (2023)",
-    director = "Jeff Grissom",
-    // timespan (hours, minutes, seconds)
-    runningTime = new TimeSpan(2, 21, 23),
-    genres = { "Comedy", "Romance" }
-};
-
-Console.WriteLine(movie.Display());
-
-Album album = new Album
-{
-    mediaId = 321,
-    title = "Greatest Album Ever, The (2020)",
-    artist = "Jeff's Awesome Band",
-    recordLabel = "Universal Music Group",
-    genres = { "Rock" }
-};
-Console.WriteLine(album.Display());
-
-Book book = new Book
-{
-    mediaId = 111,
-    title = "Super Cool Book",
-    author = "Jeff Grissom",
-    pageCount = 101,
-    publisher = "",
-    genres = { "Suspense", "Mystery" }
-};
-Console.WriteLine(book.Display());
+string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
+logger.Info(scrubbedFile);
 
 logger.Info("Program ended");
