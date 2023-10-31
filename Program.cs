ae869662b065e1361acb859d3aac5214ebfca93e
rbryan95 <berd1495@gmail.com>
2023-10-31 18:29:08 -0500

    Main Program Update with User Interface and scrub

--- Program.cs
+++ Program.cs
@@ -1,45 +1,85 @@
 ﻿﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
-// See https://aka.ms/new-console-template for more information
-string path = Directory.GetCurrentDirectory() + "\\nlog.config";
-
-// create instance of Logger
-var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
-logger.Info("Program started");
-
-string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
-logger.Info(scrubbedFile);
-MovieFile movieFile = new MovieFile(scrubbedFile);
-
-// Create a new instance of the Movie class
-Movie movie = new Movie();
-
-// Prompt the user to enter the movie title
-Console.WriteLine("Enter the movie title:");
-movie.title = Console.ReadLine();
-
-// Prompt the user to enter the movie genres
-Console.WriteLine("Enter the movie genres (separated by commas):");
-string genres = Console.ReadLine();
-movie.genres = genres.Split(',').ToList();
-
-// Prompt the user to enter the movie director
-Console.WriteLine("Enter the movie director:");
-movie.director = Console.ReadLine();
-
-// Prompt the user to enter the movie running time
-Console.WriteLine("Enter the movie running time (in minutes):");
-int runningTimeInMinutes = int.Parse(Console.ReadLine());
-movie.runningTime = TimeSpan.FromMinutes(runningTimeInMinutes);
-
-// Add the movie to the list of movies
-List<Movie> Movies = new List<Movie>();
-
-// Display all movies
-Console.WriteLine("All movies:");
-foreach (Movie m in Movies)
+class Program
 {
-    Console.WriteLine($"ID: {m.mediaId}, Title: {m.title}, Genres: {string.Join(", ", m.genres)}, Director: {m.director}, Running Time: {m.runningTime}");
-}
+    private static NLog.Logger logger = LogManager.LoadConfiguration(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
 
-logger.Info("Program ended");
+    static void Main()
+    {
+        string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
+        if (!string.IsNullOrEmpty(scrubbedFile))
+        {
+            MovieFile movieFile = new MovieFile(scrubbedFile);
+            bool continueRunning = true;
+
+            while (continueRunning)
+            {
+                Console.WriteLine("Movie Library Program");
+                Console.WriteLine("1. Display All Movies");
+                Console.WriteLine("2. Add a New Movie");
+                Console.WriteLine("3. Exit");
+                Console.Write("Enter your choice (1/2/3): ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        DisplayAllMovies(movieFile);
+                        break;
+                    case "2":
+                        Movie newMovie = GetNewMovie();
+                        if (newMovie != null)
+                        {
+                            movieFile.AddMovie(newMovie);
+                            Console.WriteLine("Movie added successfully.");
+                        }
+                        break;
+                    case "3":
+                        continueRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+
+        logger.Info("Program ended");
+    }
+
+    static Movie GetNewMovie()
+    {
+        Console.WriteLine("Enter details for the new movie:");
+        Console.Write("Title: ");
+        string title = Console.ReadLine();
+        Console.Write("Director: ");
+        string director = Console.ReadLine();
+        Console.Write("Running Time (hh:mm:ss): ");
+        if (TimeSpan.TryParse(Console.ReadLine(), out TimeSpan runningTime))
+        {
+            Movie newMovie = new Movie();
+            newMovie.title = title;
+            newMovie.director = director;
+            newMovie.runningTime = runningTime;
+            return newMovie;
+        }
+        else
+        {
+            Console.WriteLine("Invalid Running Time format. Movie not added.");
+            return null;
+        }
+    }
+
+    static void DisplayAllMovies(MovieFile movieFile)
+    {
+        Console.WriteLine("All Movies:");
+        foreach (Movie movie in movieFile.Movies)
+        {
+            Console.WriteLine(movie.Display());
+        }
+    }
+}
